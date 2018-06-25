355e6a41d387fcb0442296a8aed2c53cdd3c9caf
IrinaBabich <36416921+IrinaBabich@users.noreply.github.com>
2018-06-25 20:21:45 +0300

    Multithreading Server implementation

--- Server.java
+++ Server.java
@@ -1,4 +1,4 @@
-package com.babich.echoserver.inputoutput;
+package com.babich.echoserver.readerwriter;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -6,17 +6,46 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(3000);
-        Socket socket = serverSocket.accept();
-
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
-
-        byte[] message = new byte[100];
-        int count = bufferedInputStream.read(message);
-        bufferedOutputStream.write("echo:".getBytes());
-        bufferedOutputStream.write(message);
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            while (true) {
+                Socket client = server.accept();
+                EchoHandler handler = new EchoHandler(client);
+                handler.start();
+            }
+        } catch (Exception e) {
+            System.err.println("Exception caught: " + e);
+        }
     }
 }
 
+    // класс Thread позволяет JVM запускать потоки одновременно
+    class EchoHandler extends Thread {
+        Socket client;
 
+        EchoHandler(Socket client) {
+            this.client = client;
+        }
+
+        public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+                while (true) {
+                    String message = reader.readLine();
+                    System.out.println("Message from client: " + message);
+                    writer.write("echo: " + message);
+                    writer.newLine();
+                }
+            } catch (Exception e) {
+                System.err.println("Exception caught: client disconnected.");
+
+            } finally {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+}
